f60ede93ae19d922b795c6661bb0fa9cf338f8dd
Neeraj Samtani <neerajjsamtani@gmail.com>
2025-03-27 19:10:53 -0700

    Passing build

--- client/src/app/api/auth/[...nextauth]/route.ts
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -1,26 +1,29 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
-export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth({
-    providers: [
-        Google({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-    ],
-    session: {
-        strategy: "jwt",
-    },
-    pages: {
-        signIn: "/login",
-    },
-    debug: process.env.NODE_ENV === "development",
-    callbacks: {
-        async session({ session, token }) {
-            if (session.user) {
-                session.user.id = token.sub!;
-            }
-            return session;
+export const { handlers: { GET, POST } } = NextAuth(async (req) => {
+    console.log(req) // do something with the request
+    return {
+        providers: [
+            Google({
+                clientId: process.env.GOOGLE_CLIENT_ID!,
+                clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            }),
+        ],
+        session: {
+            strategy: "jwt",
         },
-    },
+        pages: {
+            signIn: "/login",
+        },
+        debug: process.env.NODE_ENV === "development",
+        callbacks: {
+            async session({ session, token }) {
+                if (session.user) {
+                    session.user.id = token.sub!;
+                }
+                return session;
+            },
+        },
+    };
 });
